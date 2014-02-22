78176471f9485bdc1dca7092b73cfb78a2db4b8e
Farida Campbell <faridacampbell@Faridas-MacBook-Air.local>
2014-02-22 13:41:36 -0500

    Initial Commit

---
+++ Test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Farida Campbell on 22/02/2014.
+//  Copyright (c) 2014 Farida Campbell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Test/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Test
+//
+//  Created by Farida Campbell on 22/02/2014.
+//  Copyright (c) 2014 Farida Campbell. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
